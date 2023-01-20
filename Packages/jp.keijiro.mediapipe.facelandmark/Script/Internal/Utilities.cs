adbaa7ec61611435ab92a2c1ac94d96ef60c49ee
IKEP <34697515+creativeIKEP@users.noreply.github.com>
2023-01-20 23:24:53 +0900

    code format

--- Packages/jp.keijiro.mediapipe.facelandmark/Script/Internal/Utilities.cs
+++ Packages/jp.keijiro.mediapipe.facelandmark/Script/Internal/Utilities.cs
@@ -1,25 +1,26 @@
 using Unity.Barracuda;
 using UnityEngine;
 
-namespace MediaPipe.FaceLandmark {
-
-static class IWorkerExtensions
+namespace MediaPipe.FaceLandmark
 {
-    //
-    // Retrieves an output tensor from a NN worker and returns it as a
-    // temporary render texture. The caller must release it using
-    // RenderTexture.ReleaseTemporary.
-    //
-    public static RenderTexture
-      CopyOutputToTempRT(this IWorker worker, int w, int h)
+
+    static class IWorkerExtensions
     {
-        var fmt = RenderTextureFormat.RFloat;
-        var shape = new TensorShape(1, h, w, 1);
-        var rt = RenderTexture.GetTemporary(w, h, 0, fmt);
-        using (var tensor = worker.PeekOutput().Reshape(shape))
-            tensor.ToRenderTexture(rt);
-        return rt;
+        //
+        // Retrieves an output tensor from a NN worker and returns it as a
+        // temporary render texture. The caller must release it using
+        // RenderTexture.ReleaseTemporary.
+        //
+        public static RenderTexture
+          CopyOutputToTempRT(this IWorker worker, int w, int h)
+        {
+            var fmt = RenderTextureFormat.RFloat;
+            var shape = new TensorShape(1, h, w, 1);
+            var rt = RenderTexture.GetTemporary(w, h, 0, fmt);
+            using (var tensor = worker.PeekOutput().Reshape(shape))
+                tensor.ToRenderTexture(rt);
+            return rt;
+        }
     }
-}
 
 } // namespace MediaPipe.FaceLandmark
